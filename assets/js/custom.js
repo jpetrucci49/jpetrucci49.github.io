e36bfc7e7eaf636fe39e024cbf15af2766e5c1f9
Jpetrucci49 <jpetrucci49@yahoo.com>
2018-05-28 17:32:43 -0400

    re-write js and php for mailer again

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -64,6 +64,7 @@ jQuery(function($) {
 		})
 	});
 	
+	
 	function after_form_submitted(data){
 		if(data.result == 'success'){
 			$('#frmemail').hide();
@@ -108,8 +109,39 @@ jQuery(function($) {
 			success: after_form_submitted,
 			dataType: 'json'
 		});
-
 	});
+	*/
+	// magic.js
+	$(document).ready(function() {
+		// process the form
+		$('form').submit(function(event) {
+			// stop the form from submitting the normal way and refreshing the page
+			event.preventDefault();
+			// get the form data
+			// there are many ways to get this data using jQuery (you can use the class or id also)
+			var formData = {
+				'user_name': $('input[name=user_name]').val(),
+				'user_email': $('input[name=user_email]').val(),
+				'user_msg': $('input[name=user_msg]').val()
+			};
+			// process the form
+			$.ajax({
+				type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
+				url: 'insert.php', // the url where we want to POST
+				data: formData, // our data object
+				dataType: 'json', // what type of data do we expect back from the server
+				encode: true
+			})
+			// using the done promise callback
+			.done(function(data) {
+				// log data to the console so we can see
+				console.log(data); 
+				// here we will handle errors and validation messages
+			});
+		});
+	});
+
+
 	/*
 	$("#submit").click(function(e) {
 		e.preventDefault();
--- insert.php
+++ insert.php
@@ -1,15 +1,44 @@
 <?php include_once("index.html"); ?>
 <?php
-$name = $_POST['user_name'];
-$email = $_POST['user_email'];
-$message = $_POST['user_msg'];
+// process.php
+	$errors = array();// array to hold validation errors
+	$data = array();// array to pass back data
 
-$to = "jpetrucci49@yahoo.com";
-$subject = "Form submission:";
-$body = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
-$headers = "From: " . $email;
+// validate the variables ======================================================
+// if any of these variables don't exist, add an error to our $errors array
+	if (empty($_POST['user_name']))
+		$errors['user_name'] = 'Name is required.';
+	if (empty($_POST['user_email']))
+		$errors['user_email'] = 'Email is required.';
+	if (empty($_POST['user_msg']))
+		$errors['user_msg'] = 'Superhero alias is required.';
 
-//send email
-mail($to, $subject, $body, $headers);
+// return a response ===========================================================
+// if there are any errors in our errors array, return a success boolean of false
+	if ( ! empty($errors)) {
+		// if there are items in our errors array, return those errors
+		$data['success'] = false;
+		$data['errors']  = $errors;
+	} else {
+		// if there are no errors process our form, then return a message
+		// DO ALL YOUR FORM PROCESSING HERE
+		// THIS CAN BE WHATEVER YOU WANT TO DO (LOGIN, SAVE, UPDATE, WHATEVER)
+		// show a message of success and provide a true success variable
+		$data['success'] = true;
+		$data['message'] = 'Success!';
+		
+		$name = $_POST['user_name'];
+		$email = $_POST['user_email'];
+		$message = $_POST['user_msg'];
 
+		$to = "jpetrucci49@yahoo.com";
+		$subject = "Form submission:";
+		$body = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
+		$headers = "From: " . $email;
+
+	}
+	//send email
+	mail($to, $subject, $body, $headers);
+	// return all our data to an AJAX call
+	echo json_encode($data);
 ?>
